9c9e576c7e87688f54bcc27efad0b08df1470fa0
milk <gongzhenyu40@qq.com>
2021-06-08 23:28:10 +0800

    pictureSplicing

--- pictureSplicing/README.md
+++ pictureSplicing/README.md
@@ -1,21 +1,24 @@
 ## 作用
 
-+ 将传入图片链接按照 [传入参数](#opts) ，按照 *瀑布流* 的排布方式拼接
++ 将传入图片链接按照 [传入参数](#opts) ，按照 *瀑布流* 的排布方式进行拼接
 
 
 
-## 基本用法
+## 用法
 
 ```javascript
-new PictureSplicing({
-    urls: [ 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2496571732,442429806&fm=26&gp=0.jpg', 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2151136234,3513236673&fm=26&gp=0.jpg',  'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3566088443,3713209594&fm=26&gp=0.jpg',   'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2676935521,922112450&fm=11&gp=0.jpg'],
-    width: 200,
-    row: 3,
-    maxHeight: 600,
-    onload: function (loaded, total) { },
-    onerror: function (url) { },
-    cb: function (canvas) { }
+const pictureSplic = new PictureSplicing({
+  urls: ['https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2496571732,442429806&fm=26&gp=0.jpg', 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2151136234,3513236673&fm=26&gp=0.jpg', 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3566088443,3713209594&fm=26&gp=0.jpg', 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2676935521,922112450&fm=11&gp=0.jpg'],
+  width: 200,
+  row: 2,
+  maxHeight: 600
 });
+
+pictureSplic.loadSuccess((loaded, total) => { })
+
+pictureSplic.loadFail(url => { })
+
+pictureSplic.finish(canvas => { })
 ```
 
 
@@ -24,15 +27,24 @@ new PictureSplicing({
 
 + Object
 
-|    键名     |   类型   | 必填 |                             描述                             |
-| :---------: | :------: | :--: | :----------------------------------------------------------: |
-|   `urls`    |  Array   |  √   |                     要拼接的图片链接数组                     |
-|   `width`   |  Number  |  √   |                        每张图片的宽度                        |
-|    `row`    |  Number  |  √   |                        拼接一共多少列                        |
-| `maxHeight` |  Number  |  √   |          以 `width` 为宽缩放后，每张图片的最大高度           |
-|  `onload`   | Function |  ×   | 每张图片load成功回调<br />回调形参 - 已完成：`loaded`；总数：`total` |
-|  `onerror`  | Function |  ×   |   每张图片load失败回调<br />回调形参 - 失败图片链接：`url`   |
-|    `cb`     | Function |  ×   |    拼接完成后回调<br />回调形参 - 拼接后的画布：`canvas`     |
+|    键名     |  类型  | 默认 |                   描述                    |
+| :---------: | :----: | :--: | :---------------------------------------: |
+|   `urls`    | Array  | 必填 |           要拼接的图片链接数组            |
+|   `width`   | Number | 200  |              每张图片的宽度               |
+|    `row`    | Number |  2   |              拼接一共多少列               |
+| `maxHeight` | Number | 600  | 以 `width` 为宽缩放后，每张图片的最大高度 |
+
+
+
+## 事件注册
+
++ `loadSuccess` - 每张图 load 成功的回调
+  + `loaded` - 已 load 图片张数
+  + `total` - 图片总张数
++ `loadFail` - 每张图 load 失败的回调
+  + `url` - load 失败图片链接
++ `finish` - 完成后回调
+  + `canvas` - 拼接完成后的 canvas
 
 
 
@@ -43,3 +55,4 @@ new PictureSplicing({
   + `encoderOptions` - 在指定图片格式为 `image/jpeg 或` `image/webp的情况下，可以从 0 到 1 的区间内选择图片的质量`。如果超出取值范围，将会使用默认值 `0.92`。
 + 获取 `Blob` - `canvas.toBlob(callback, type, encoderOptions)`
   + `callback` - 回调函数，可获得一个单独的 `Blob` 对象参数。
+
--- pictureSplicing/index.js
+++ pictureSplicing/index.js
@@ -1,139 +1,162 @@
-function PictureSplicing(opts) {
-  this.opts = opts;
-  this.datas = [];
-  this.loadCount = 0;
-  this.line = [];
-  for (var i = 0; i < this.opts.row; i++) {
-    this.line.push(0);
+function noop() { }
+
+class PictureSplicing {
+  constructor(opts) {
+    this.opts = {
+      ...{
+        width: 200,
+        row: 2,
+        maxHeight: 600,
+        onload: noop,
+        onerror: noop,
+        cb: noop
+      },
+      ...opts
+    };
+
+    this.urls = this.opts.urls;
+    this.width = this.opts.width;
+    this.maxHeight = this.opts.maxHeight;
+
+    this.init();
   }
-  this.allHeight = 0;
 
-  this.canvas = document.createElement('canvas');
-  this.ctx = this.canvas.getContext('2d');
+  init() {
+    // 组装后数据
+    this.datas = [];
+    // successLoad 计数
+    this.loadCount = 0;
+    // 列数组
+    this.rows = new Array(this.opts.row).fill(0);
 
-  this.readyLoad();
-}
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
 
-PictureSplicing.prototype.readyLoad = function () {
-  for (var i = 0; i < this.opts.urls.length; i++) {
-    var img = new Image();
-    img.src = this.opts.urls[i];
-    img.crossOrigin = 'anonymous';
-    img.index = i;
-    img.errorNum = 0;
-    img.onload = this.onLoad.bind(this, img);
-    img.onerror = this.onerror.bind(this, img);
+    this.readyLoad();
+  }
 
-    this.datas.push({
-      img: img,
-      w: this.opts.width
+  readyLoad() {
+    this.urls.forEach((item, index) => {
+      const img = new Image();
+      img.src = item;
+      img.crossOrigin = 'anonymous';
+      img.errorNum = 0;
+
+      img.onload = this.onLoad.bind(this, img, index);
+      img.onerror = this.onerror.bind(this, img, index);
+
+      this.datas.push({
+        img: img,
+        w: this.width
+      });
     });
   }
-}
 
-PictureSplicing.prototype.onLoad = function (img) {
-  this.loadCount++;
-
-  this.datas[img.index].h = this.opts.width / (img.width / img.height);
-
-  this.opts.onload && this.opts.onload(this.loadCount, this.opts.urls.length);
-
-  this.loadCount === this.opts.urls.length && this.finishLoad();
-}
-
-PictureSplicing.prototype.onerror = function (img) {
-  var src = img.src;
-  var addType = '?id=';
-  if (src.indexOf("?") > -1) {
-    if (src.indexOf("?id=") > -1) {
-      src = src.substring(0, src.indexOf("?id="));
-    } else {
-      if (src.indexOf("&id=") > -1) {
-        src = src.substring(0, src.indexOf("&id="));
-      }
-      addType = '&id=';
-    }
+  loadSuccess(fn) {
+    this.opts.onload = fn;
   }
 
-  if (img.errorNum >= 3) {
+  loadFail(fn) {
+    this.opts.onerror = fn;
+  }
+
+  finish(fn) {
+    this.opts.cb = fn;
+  }
+
+  onLoad(img, index) {
     this.loadCount++;
 
-    this.opts.onerror && this.opts.onerror(src);
+    this.datas[index].h = this.width / (img.width / img.height);
 
-    this.datas[img.index].error = true;
+    this.opts.onload(this.loadCount, this.opts.urls.length);
 
     this.loadCount === this.opts.urls.length && this.finishLoad();
-    return
   }
 
-  img.errorNum += 1;
-  img.src = src + addType + parseInt(Math.random() * 1000000000000000, 10);
-}
+  onerror(img, index) {
+    const { src, addType } = this.removeId(img.src);
 
-PictureSplicing.prototype.finishLoad = function () {
-  this.arrangement();
+    if (img.errorNum >= 3) {
+      this.loadCount++;
+      this.opts.onerror(src);
 
-  this.canvas.width = this.opts.width * this.opts.row;
-  this.canvas.height = this.allHeight;
+      this.datas[index].error = true;
 
-  this.draw();
-
-  this.opts.cb && this.opts.cb(this.canvas);
-}
-
-PictureSplicing.prototype.arrangement = function () {
-  for (var i = this.datas.length - 1; i >= 0; i--) {
-    (this.datas[i].error || this.datas[i].h >= this.opts.maxHeight) && this.datas.splice(i, 1);
-  }
-
-  this.datas.sort(function (a, b) {
-    return b.h - a.h;
-  });
-
-  for (var i = 0; i < this.datas.length; i++) {
-    var min = Math.min.apply(this, this.line);
-    var index = this.line.indexOf(min);
-
-    this.datas[i].x = index * this.opts.width;
-    this.datas[i].y = this.line[index];
-    this.line[index] += this.datas[i].h;
-
-    if (this.line[index] > this.allHeight) {
-      this.allHeight = this.line[index];
+      this.loadCount === this.opts.urls.length && this.finishLoad();
+      return
     }
+
+    img.errorNum += 1;
+    img.src = src + addType + parseInt(Math.random() * 1000000000000000, 10);
   }
 
-  var arr = [];
-
-  for (var i = 0; i < this.opts.row; i++) {
-    arr[i] = [];
-    for (var j = 0; j < this.datas.length; j++) {
-      if (this.datas[j].x === i * this.opts.width) {
-        arr[i].push(this.datas[j]);
+  removeId(src) {
+    let addType = '?id=';
+    if (src.indexOf("?") > -1) {
+      if (src.indexOf("?id=") > -1) {
+        src = src.substring(0, src.indexOf("?id="));
+      } else {
+        if (src.indexOf("&id=") > -1) {
+          src = src.substring(0, src.indexOf("&id="));
+        }
+        addType = '&id=';
       }
     }
-  }
 
-  for (var i = 0; i < arr.length; i++) {
-    arr[i].sort(function (a, b) {
-      return a.h - b.h;
-    });
-
-    var y = 0;
-    var x = (arr.length - i - 1) * this.opts.width;
-    for (var j = 0; j < arr[i].length; j++) {
-      arr[i][j].x = x;
-      arr[i][j].y = y;
-      y += arr[i][j].h;
+    return {
+      src,
+      addType
     }
   }
-}
 
-PictureSplicing.prototype.draw = function () {
-  this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  finishLoad() {
+    this.arrangement();
 
-  for (var i = 0; i < this.datas.length; i++) {
-    var data = this.datas[i];
-    this.ctx.drawImage(data.img, data.x, data.y, data.w, data.h);
+    this.canvas.width = this.width * this.opts.row;
+    this.canvas.height = Math.max.apply(this, this.rows);
+
+    this.draw();
+
+    this.opts.cb(this.canvas);
+  }
+
+  arrangement() {
+    this.datas = this.datas.filter(item => !item.error && item.h <= this.maxHeight);
+
+    this.datas.sort((a, b) => b.h - a.h);
+
+    this.datas.forEach(item => {
+      const min = Math.min.apply(this, this.rows);
+      const index = this.rows.indexOf(min);
+
+      item.x = index * this.width;
+      item.y = this.rows[index];
+
+      this.rows[index] += item.h;
+    });
+
+    let arr = [];
+    for (var i = 0; i < this.opts.row; i++) {
+      arr.push(this.datas.filter(item => item.x === i * this.width));
+    }
+
+    arr.forEach((item, index) => {
+      item.sort((a, b) => a.h - b.h);
+
+      let y = 0;
+      let x = (arr.length - index - 1) * this.width;
+      item.forEach(data => {
+        data.y = y;
+        data.x = x;
+        y += data.h;
+      })
+    })
+  }
+
+  draw() {
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.datas.forEach(item => this.ctx.drawImage(item.img, item.x, item.y, item.w, item.h));
   }
 }
