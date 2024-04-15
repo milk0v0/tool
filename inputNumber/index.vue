f6f47565194ac66890444853ef22878c9df1dcee
milk <gongzhenyu40@qq.com>
2024-04-15 16:19:51 +0800

    feat: inputNumber

---
+++ inputNumber/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <input v-model="value" type="text" @input="handleInput" @blur="handleBlur" />
+</template>
+
+<script>
+export default {
+  name: 'InputNumber',
+
+  data() {
+    return {
+      value: 0,
+    };
+  },
+
+  methods: {
+    handleInput(e) {
+      const input = e.target;
+      const { value } = input;
+      const index = input.selectionStart - 1;
+      const keyIn = value[index];
+
+      if (
+        // 小数点，但不是只有一个
+        (keyIn === '.' && value.match(/\./g).length > 1) ||
+        // 非数字小数点
+        /[^\d.]/.test(keyIn)
+      ) {
+        const before = value.substring(0, index);
+        const after = value.substring(index + 1);
+        this.height = `${before}${after}`;
+
+        // 恢复光标位置
+        this.$nextTick(() => {
+          input.setSelectionRange(index, index);
+        });
+      }
+    },
+
+    handleBlur() {
+      this.height = `${+this.height}`;
+    },
+  },
+};
+</script>
