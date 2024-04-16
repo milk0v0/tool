960f135af0944ba3f8db88c712e8963ebf825351
milk <gongzhenyu40@qq.com>
2024-04-16 10:01:54 +0800

    feat: inputNumber

--- inputNumber/index.vue
+++ inputNumber/index.vue
@@ -1,15 +1,36 @@
 <template>
-  <input v-model="value" type="text" @input="handleInput" @blur="handleBlur" />
+  <input v-model="model" v-bind="$attrs" type="text" v-on="$listeners" @input="handleInput" @blur="handleBlur" />
 </template>
 
 <script>
 export default {
   name: 'InputNumber',
 
-  data() {
-    return {
-      value: 0,
-    };
+  model: {
+    prop: 'modelValue',
+    event: 'changeVal',
+  },
+
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    int: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.modelValue;
+      },
+      set(newVal) {
+        this.$emit('changeVal', newVal);
+      },
+    },
   },
 
   methods: {
@@ -19,25 +40,38 @@ export default {
       const index = input.selectionStart - 1;
       const keyIn = value[index];
 
-      if (
-        // 小数点，但不是只有一个
-        (keyIn === '.' && value.match(/\./g).length > 1) ||
-        // 非数字小数点
-        /[^\d.]/.test(keyIn)
-      ) {
+      let isDelete;
+
+      switch (keyIn) {
+        case '-':
+          isDelete = index !== 0 || value.match(/-/g).length > 1;
+          break;
+        case '.':
+          isDelete = this.int || value.match(/\./g).length > 1;
+          break;
+        default:
+          isDelete = /\D/.test(keyIn);
+      }
+
+      if (isDelete) {
         const before = value.substring(0, index);
         const after = value.substring(index + 1);
-        this.value = `${before}${after}`;
+        this.model = `${before}${after}`;
+        input.value = this.model;
 
         // 恢复光标位置
         this.$nextTick(() => {
           input.setSelectionRange(index, index);
         });
       }
+
+      this.$emit('input', this.model);
     },
 
     handleBlur() {
-      this.value = `${+this.value}`;
+      this.model = `${+this.model}`;
+
+      this.$emit('blur', this.model);
     },
   },
 };
